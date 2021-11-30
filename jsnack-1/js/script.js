0a8e71c485692d8ea7681b61ea816b60588e73ed
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-30 10:49:12 +0100

    controllo-doppioni

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -6,11 +6,19 @@ let nomi = ['Leonardo', 'Matteo', 'Luca', 'Martina', 'Veronica', 'Irene'];
 // dichiaro array cognomi
 let cognomi = ['Rossi', 'Ferrari', 'Bianchi', 'Romano', 'Gallo', 'Costa'];
 
+// dichiaro array vuoto per lista invitati
+
+let listaInvitati = [];
+
 // ciclo for per controllare l'array
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 20; index++) {
   // genero casualmente nomi e cognomi partendo dagli array
-  let listaInvitati = nomi[Math.floor(Math.random() * nomi.length)] + cognomi[Math.floor(Math.random() * cognomi.length)];
+  let ospite = nomi[Math.floor(Math.random() * nomi.length)] + ' ' + cognomi[Math.floor(Math.random() * cognomi.length)];
+
+  if (!listaInvitati.includes(ospite)) {
+    listaInvitati.push(ospite);
+  }
   // stampo in console la lista degli invitati
-  console.log(listaInvitati);
 }
+console.log(listaInvitati);
 
