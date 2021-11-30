1dca8cf5f0b53549eec7fe4df165c934b052969f
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-30 09:41:29 +0100

    add-for-with-list

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,3 +1,11 @@
 /* jsnack 1
 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati. */
 
+let nomi = ['Leonardo', 'Matteo', 'Luca', 'Martina', 'Veronica', 'Irene'];
+let cognomi = ['Rossi', 'Ferrari', 'Bianchi', 'Romano', 'Gallo', 'Costa'];
+
+for (let index = 0; index < 5; index++) {
+  let listaInvitati = nomi[Math.floor(Math.random() * nomi.length)] + cognomi[Math.floor(Math.random() * cognomi.length)];
+  console.log(listaInvitati);
+}
+
